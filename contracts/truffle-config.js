fe89c4385ce6650cba53f5179c400492551b4e7a
SilentKnight87 <petrojoash@yahoo.com>
2021-12-15 17:18:02 -0500

    Added functionality to add and save profile

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -42,7 +42,7 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
+     host: "0.0.0.0",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     }
@@ -104,13 +104,13 @@ module.exports = {
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
--- frontend/main_test.js
+++ frontend/main_test.js
@@ -1,15 +1,21 @@
 /* Moralis init code */
-const serverUrl = "https://masdi5ewxhhw.usemoralis.com:2053/server";
-const appId = "1bZMC0KVQeXFRu5HojPeKBA5GJ57psWKX3MJ3Kkj";
+const serverUrl = "https://cfcle0kimiqg.usemoralis.com:2053/server";
+const appId = "Swj06DPVbh2uaZuBKnMSk7C0zkCMdx1XnEY0SIVY";
 const TOKEN_CONTRACT_ADDRESS = "0x4a2c549180cF41A5bCd73779De12E5FC9218A298"
 Moralis.start({ serverUrl, appId });
+const tokenContract = new web3.eth.Contract(tokenContractAbi, TOKEN_CONTRACT_ADDRESS);
+
+//Assigns the username and email values to a var
+// let userUsernameField = document.getElementById("txtUsername");
+// let userEmailField = document.getElementById("txtEmail");
+
 /* Authentication code */
 
-initUser = async () => {
-  if (await Moralis.User.current()){
-    hideElement
-  }
-}
+// initUser = async () => {
+//   if (await Moralis.User.current()){
+//     hideElement
+//   }
+// }
 console.log('It got clicked')
 async function login() {
   let user = Moralis.User.current();
@@ -25,11 +31,74 @@ async function login() {
         console(error);
       });
   }
+  return user;
 }
 async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
 }
+
+async function openUserInfo()  {
+  const userUsernameField = document.getElementById("txtUsername");
+  const userEmailField = document.getElementById("txtEmail");
+  user = await Moralis.User.current();
+  if (user){
+    if(location.href != 'http://127.0.0.1:5502/frontend/signup.html'){
+      location.href = 'signup.html'
+    };
+
+    const email = user.get('email');
+    if(email){
+      userUsernameField.value = email;
+    }else{
+      userEmailField.value = "";
+    }
+
+    userUsernameField.value = user.get('username')
+
+    // location.href = 'signup.html';
+  } else{
+    login(); 
+  }
+}
+
+async function saveUserInfo() {
+  const userUsernameField = document.getElementById("txtUsername");
+  const userEmailField = document.getElementById("txtEmail"); 
+  user = await Moralis.User.current();
+  user.set('email', userEmailField.value);
+  user.set('username', userUsernameField.value);
+  // if(user){
+  //   const email = user.get('email');
+  //   if(email){
+  //     userEmailField.value = email;
+  //   }else{
+  //     userEmailField.value = "";
+  //   }
+  //   userUsernameField.value = user.get('username')
+  //   console.log('this is username',userUsernameField.value)
+  //   user.set('email', userEmailField.value);
+  //   user.set('username', userUsernameField.value);
+    await user.save();
+    alert("User info saved successfully");
+    openUserInfo();
+  // }else{
+  //   login(); 
+  // }
+
+}
+
+// userInfo
+
+// mintNFT = async (metadataUrl) => {
+//   const receipt = await tokenContract.methods.createItem(metadataUrl).send({from: ethereum.selectedAddress})
+// console.log(receipt);
+// return receipt.events.Trancfer.returnValues.tokenId;
+// }
+
+
 // document.getElementById("wallet_connect").onclick = login;
 // document.getElementById("btnUserInfo").onclick = logOut;
-// const userInfo = document.getElementById("userInfo");
+// const userUsernameField = document.getElementById("txtUsername");
+// const userEmailField = document.getElementById("txtEmail");
+
